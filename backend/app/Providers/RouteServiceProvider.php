9ae1a090d374a1c560865c7c916869522e1127b6
AdriCQ <adrian.capote95@zoho.com>
2022-07-09 13:43:47 -0400

    Restart service provider

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -39,6 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->prefix('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
